5f2904e2754acd93cc6776b9ee0437575483c690
Anthony Parrino <anthony21parrino21@gmail.com>
2020-11-30 10:55:23 -0500

    created database

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+/* If database already exsits drop it and overwrite it */
+DROP DATABASE IF EXISTS burgers_db;
+
+/* Creating and using database */
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+/* New tables with primary keys that auto-increments and ids that refer to other tables */
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burgers VARCHAR(100) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burgers) VALUES ('Krabby Patty');
+INSERT INTO burgers (burgers) VALUES ('Bobs Burger');
+INSERT INTO burgers (burgers) VALUES ('Cartmen Burger');
+INSERT INTO burgers (burgers) VALUES ('Mondo Burger');
+INSERT INTO burgers (burgers) VALUES ('Honker Burger');
+INSERT INTO burgers (burgers) VALUES ('Krusty Burger');
+INSERT INTO burgers (burgers) VALUES ('Big Mac');
